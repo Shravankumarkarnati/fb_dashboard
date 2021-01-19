de69b75c77e51db0b4cbfcef8eef142615f885bd
Shravan Kumar Karnati <shravan.kumar063@gmail.com>
2021-01-19 13:55:31 -0500

    chore(file structure): structured files and folders
    To maintain consistency across the team -> Inspired from react-boilerplate

---
+++ src/app/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "./components/footer/footer";
+import Header from "./components/Header/header";
+
+function App() {
+  // reportWebVitals(console.log);
+  return (
+    <div className="App" data-testid="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app/components/Header/header.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+interface HeaderProps {}
+
+const Header: React.FC<HeaderProps> = () => <div>Header</div>;
+export default Header;
