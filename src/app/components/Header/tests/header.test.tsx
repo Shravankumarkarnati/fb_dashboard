18a033b579fa0baa74ebd31dcc6d8a5641e0388d
Shravan Kumar Karnati <shravan.kumar063@gmail.com>
2021-01-20 00:44:08 -0500

    chore: stylelint integrated

--- src/app/components/Header/tests/header.test.tsx
+++ src/app/components/Header/tests/header.test.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import Header from "../header";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Header from '../header';
 
-describe("Header Tests", () => {
-  it("Header Renders and Is Not Empty", () => {
+describe('Header Tests', () => {
+  it('Header Renders and Is Not Empty', () => {
     render(<Header />);
-    const HeaderContainer = screen.getByTestId("Header");
+    const HeaderContainer = screen.getByTestId('Header');
     expect(HeaderContainer).not.toBeEmptyDOMElement();
   });
 });
